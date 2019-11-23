0f64e4fcb65fd04584b4b512018689b8ee81a1ba
工藤千寛 <kudousenhiroshi@ChihiroKudo.local>
2019-11-23 15:35:14 +0900

    Added map type switching function

--- MyMap/ViewController.swift
+++ MyMap/ViewController.swift
@@ -81,5 +81,25 @@ class ViewController: UIViewController , UITextFieldDelegate{
         return true
     }
     
+    @IBAction func changeMapButton(_ sender: Any) {
+        
+        //mapTypeプロパティ値をトグル
+        //標準 → 航空写真 → 航空写真 + 標準
+        // → 3D Flyover 3D Flyover + 標準
+        // → 交通機関
+        if dispMap.mapType == .standard {
+            dispMap.mapType = .satellite
+        } else if dispMap.mapType == .satellite {
+            dispMap.mapType = .hybrid
+        } else if dispMap.mapType == .hybrid {
+            dispMap.mapType = .satelliteFlyover
+        } else if dispMap.mapType == .satelliteFlyover {
+            dispMap.mapType = .hybridFlyover
+        } else if dispMap.mapType == .hybridFlyover {
+            dispMap.mapType = .mutedStandard
+        } else {
+            dispMap.mapType = .standard
+        }
+    }
 }
 
